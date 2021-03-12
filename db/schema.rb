7bb10b6825741223c8d0c5e3ccdc865a8c395d2e
sharath234 <ch.sharath99@gmail.com>
2021-03-12 15:24:21 +0530

    added comments to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_065949) do
+ActiveRecord::Schema.define(version: 2021_03_12_065402) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+    t.integer "parent_id"
+  end
 
   create_table "friends", force: :cascade do |t|
     t.string "name"
@@ -22,6 +31,13 @@ ActiveRecord::Schema.define(version: 2021_03_08_065949) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_friends_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
